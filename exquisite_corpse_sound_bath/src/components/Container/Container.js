665c6aa87ee3a82566def7192c14255ad94f3cf4
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-02-26 00:28:40 -0800

    Added Header

---
+++ exquisite_corpse_sound_bath/src/components/Container/Container.js
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container'
+import "./style.css";
+import PropTypes from 'prop-types'
+
+
+const Container = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Container
---
+++ exquisite_corpse_sound_bath/src/components/Header/Jumbotron.js
@@ -0,0 +1,35 @@
+import "./style.css";
+import PropTypes from 'prop-types'
+import Image from 'react-bootstrap/Image'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+
+
+const Header = ({title,instructions}) => {
+    const headingStyle = {
+        color: 'Black',
+    }
+
+    return (
+<Jumbotron className = "Jumbotron" style={headingStyle}>
+
+  <h1>{title}</h1>
+  <p>
+    {instructions}
+  </p>
+  <p>
+    <Button variant="secondary">Let's Get Started</Button>
+  </p>
+</Jumbotron>
+  
+
+    )
+}
+Header.defaultProps = {
+    title: 'Exquisite Corpse Sound Bath ',
+    instructions: '"Record your sound & mix your music or listen to your favorite music"'
+  }
+Header.prototype = {
+    title: PropTypes.string.isRequired
+  }
+export default Header
