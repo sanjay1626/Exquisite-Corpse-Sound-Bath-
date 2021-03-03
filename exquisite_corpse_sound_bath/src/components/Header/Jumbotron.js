955b186d53aee1d95d82d61ddd2898a5483db6cd
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-03-02 21:52:00 -0800

    Lets Get Started button working routes to Login page

--- exquisite_corpse_sound_bath/src/components/Header/Jumbotron.js
+++ exquisite_corpse_sound_bath/src/components/Header/Jumbotron.js
@@ -2,20 +2,14 @@ import "./style.css";
 import PropTypes from 'prop-types'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
-
-
-
+import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
+import Main from '../Main/Main'
+import Login from '../Pages/SignIn/SignIn'
 const Header = ({title,instructions}) => {
     const headingStyle = {
         color: 'Black',
     };
  
-const onClick =() =>{
-  console.log("Clicked")
-     
-   
-
-}
 
 
     return (
@@ -26,7 +20,10 @@ const onClick =() =>{
     {instructions}
   </p>
   <p>
-    <Button onClick ={onClick} variant="secondary">Let's Login</Button>
+  <Button><Link to="/login">Let's Get Started!</Link></Button>
+
+   
+   
   </p>
 </Jumbotron>
   
