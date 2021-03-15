c27570d692e34f7e9893305aec5504bf578f1404
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-03-15 13:53:07 -0700

    update

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const app = express()
 //middleware receive info. from frontend in json format
 app.use(express.json())
 
-mongoose.connect('', {
+mongoose.connect('mongodb+srv://sanjay1626:Stormer1626$@cluster0.bylwq.mongodb.net/exquisite-corpse-db?retryWrites=true&w=majority', {
     useNewUrlParser: true,
+    
 })
 
 
