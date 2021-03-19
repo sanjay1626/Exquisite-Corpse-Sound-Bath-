fb3eeef9660e7df19f4ad7c775ea7bbee29bcb6e
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-03-18 20:20:39 -0700

    Search API

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {SearchForm} from '../SearchForm'
 import { userActions } from '../_actions';
-class HomePage extends React.Component {
-
+import axios from 'axios'
+class HomePage extends Component {
+    constructor( props ) {
+        super( props );
+       this.handleOnInputChange = this.handleOnInputChange.bind(this);
+        this.state = {
+          query: '',
+          results:{},
+          loading: false,
+          message: '',
+        };
+        this.cancel = '';
+      }
     componentDidMount() {
         this.props.getUsers();
     }
@@ -12,15 +22,70 @@ class HomePage extends React.Component {
     handleDeleteUser(id) {
         return (e) => this.props.deleteUser(id);
     }
+//function to fetch
+fetchSearchResults(updatedPageNo = '', query){
+    const pageNumber = updatedPageNo ? `&page=${pageNumber}` : '';
+    const searchUrl = `https://pixabay.com/api/?key=12413278-79b713c7e196c7a3defb5330e&q=${query}${pageNumber}`
+        if( this.cancel){
+            this.cancel.cancel();
+        }
+        this.cancel = axios.CancelToken.source();
+
+        axios.get(searchUrl, {
+            cancelToken: this.cancel.token
+        })
+            .then(res =>{
+                const resultNotFoundMsg = !res.data.hits.length
+                    ?'There are no more search results. Please try a new search.' : '';
+                this.setState({
+                    results: res.data.hits,
+                    message: resultNotFoundMsg,
+                    loading: false,
+                })
+            })
+            .catch(error =>{
+                if (axios.isCancel(error) || error){
+                    this.setState({
+                        loading : false,
+                        message: 'Failed to fecth results. Please check network'
+                    })
+                }
+            })
+      };
+
 //onChange Event
-handleOnInputChange(event){
+handleOnInputChange (event){
 	const query = event.target.value;
-            this.setState({ query, loading: true, message: ''  } );
+    this.setState({query: query, loading: true, message: ''}, ()=>{
+        this.fetchSearchResults( 1, query);
+    });
+          
 };
 
+renderSearchResults() {
+	const {results} = this.state;
 
+	if (Object.keys(results).length && results.length) {
+		return (
+			<div className="results-container">
+				{results.map((result) => {
+					return (
+						<a key={result.id} href={result.previewURL} className="result-items">
+							<h6 className="image-username">{result.name}</h6>
+							<div className="image-wrapper">
+								<img className="image" src={result.previewURL} alt={result.name}/>
+							</div>
+						</a>
+					);
+				})}
+			</div>
+		);
+	}
+};
     render() {
         const { user, users } = this.props;
+        const {query} = this.state
+        console.warn(this.state)
         return (
             <div >
                     
@@ -33,17 +98,23 @@ handleOnInputChange(event){
                 <div className = "container h-100"> 
                     <div className="d-flex justify-content-center h-100">
                         <div className="searchbar">
+                            {/* <label className ="seacrch-label" htmlFor="search-input"></label> */}
                             <input className ="search_input" 
                             type="text" 
                             placeholder="Search.." 
+                            value={query}
                             id="search-input"
-                            value=""
+                            name="query"
                             onChange={this.handleOnInputChange}></input>
-                            <button href='#' className="search_icon"><i className="fa fa-search"></i></button>
+                            
+                             {/*  */}
+                            
+                            <a href='#' className="search_icon"><i className="fa fa-search"></i></a>
                         </div>
+                      
                     </div>
                 List of popular music with image 
-          
+              {this.renderSearchResults()}
               </div>
               
               <hr></hr>
--- client/src/SearchForm/SearchForm.jsx
+++ client/src/SearchForm/SearchForm.jsx
@@ -1,13 +1,61 @@
-import React from "react";
-
-class SearchForm extends React.Component {
-  redner() {
-    return (
-      <div>
-        <h1>Search Form</h1>
-      </div>
-    );
+import React from 'react'
+import axios from  'axios'
+class SeacrhForm extends React.Component {
+  constructor( props ) {
+    super( props );
+  
+    this.state = {
+      query: '',
+      results:{},
+      loading: false,
+      message: '',
+    };
+    this.cancel = '';
   }
-}
-export default SearchForm;
+  render(){
+    return(
+         
+    )
+  }
+   fetchSearchResults (updatedPageNo = '', query ){
 
+    const pageNumber = updatedPageNo ? `&page=${updatedPageNo}` : '';
+  
+    // By default the limit of results is 20
+    const searchUrl = `https://shazam.p.rapidapi.com/search/?key=c84a11a37dmsh97101e52facfc63p160003jsna7983511d133=${query}${pageNumber}`;
+  
+    if (this.cancel) {
+      // Cancel the previous request before making a new request
+      this.cancel.cancel();
+    }
+    // Create a new CancelToken
+    this.cancel = axios.CancelToken.source();
+  
+    axios
+      .get(searchUrl, {
+        cancelToken: this.cancel.token,
+      })
+      .then((res) => {
+        const resultNotFoundMsg = !res.data.hits.length
+          ? 'There are no more search results. Please try a new search.'
+          : '';
+  
+        this.setState({
+          results: res.data.hits,
+          message: resultNotFoundMsg,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        if (axios.isCancel(error) || error) {
+          this.setState({
+            loading: false,
+            message: 'Failed to fetch results.Please check network',
+          });
+        }
+      });
+  };
+ 
+};
+
+export default SearchForm
