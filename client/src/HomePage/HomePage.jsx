3814dbe2eddc58f0ff5452609584af9fc6eaaace
Sanjay Gonsalves <snjgonsalves@gmail.com>
2021-03-23 16:48:55 -0700

    Deploy

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -128,13 +128,16 @@ class HomePage extends Component {
             <div className="item active">
               <img src="https://www.cifras.com.br/arquivos/artistas/l/led_zeppelin_02_big.jpg" /> 
               <div class="carousel-caption">
-                <h4><a href="#">Lorem ipsum dolor sit amet consetetur sadipscing</a></h4>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>
+                <h4><a href="#">LedZeppelin</a></h4>
+                <p>Led Zeppelin were an English rock band formed in London in 1968. The group consisted of vocalist Robert Plant, guitarist Jimmy Page, bassist/keyboardist John Paul Jones, and drummer John Bonham.</p>
             </div>
             </div>{/* End Item */}
             <div className="item">
               <img src="https://jazzdesk.files.wordpress.com/2019/11/ron_carter_danny_simmons_brown_beatnik_tomes.jpg" />
-             
+              <div class="carousel-caption">
+                <h4><a href="#">Ron Carter & Danny Simmons</a></h4>
+                <p>The Brown Beatnik Tomes – Live at BRIC House is a unique and powerful collaboration between poet/artist Danny Simmons and legendary jazz bassist Ron Carter. </p>
+            </div>
             </div>{/* End Item */}
             <div className="item">
               <img src="https://floridatheatre.showare.com/uplimage/TicketBanner-CALZeppelinII.JPG" />
